017b64347b8cfa41bed7d87dcf58ed1c1fa75abd
Callan Merifield <callan.merifield@gmail.com>
2026-03-04 11:17:02 +0200

    First Commit to python api project

---
+++ main.py
@@ -0,0 +1,23 @@
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
+
+app = FastAPI()
+
+#Add middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api")
+def read_root():
+    return {"message": "Testing my api"}
+
+# Run server
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=6767)
